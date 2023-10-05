27f66503fd8b219174609778945c669e32830bd4
Sunkanmi <sunkanmi.oguntimehin@optisoft.ng>
2023-10-05 13:59:02 +0100

    commit changes

--- src/routes/stripeRoutes/index.js
+++ src/routes/stripeRoutes/index.js
@@ -2,12 +2,17 @@
 const express = require("express");
 const { webCreateCheckout } = require("../../controllers/stripeController/webStripe");
 const { authMiddleware } = require("../../middlewares/authMiddlewares");
-const { creatMobilePaymentsIntent } = require("../../controllers/stripeController");
+const { creatMobilePaymentsIntent, stripeWebHook } = require("../../controllers/stripeController");
 
 const stripeRouter = express.Router();
 
 
 stripeRouter.post("/create-checkout-session", authMiddleware, webCreateCheckout)
 stripeRouter.post("/intent", authMiddleware, creatMobilePaymentsIntent);
+stripeRouter.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebHook
+);
 
 module.exports = {stripeRouter}
